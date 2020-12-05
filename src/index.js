fcabe10b81e00dd94133c47cb090d3a2388acc9d
sonali <blsonalib@gmail.com>
2020-12-05 19:58:55 +0530

    Changes and add Multiple element in render in the form of array if version is V16 above

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Render method wants only single element if want multiple element in render method the we can use div element
-// In render method we can use multiple element in the form of arrays
+// In render method we can use multiple element in the form of arrays if used above v16
 ReactDOM.render(
-  <div>
- <h1>Hello World</h1>
-  <p>This is paragraph </p>
-  </div>,
+//   <div>
+//  <h1>Hello World</h1>
+//   <p>This is paragraph </p>
+//   </div>
+[<h1>Hello World</h1>  ,<p>This is paragraph </p>, <h1>This is me</h1> ],
   document.getElementById('root')
 );
 
