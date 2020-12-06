878e81f850d1606fa80c7f19dd56182ba7e3ecaf
sonali <blsonalib@gmail.com>
2020-12-06 18:09:07 +0530

    Added Literals used in react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fname = "sonali";
+const fName = "sonali";
 const lName = "Bankar"
 
 ReactDOM.render(
   <> 
-    <h1>My First_Name is {fname}</h1>
-    <h1>My Last_Name is { lName}</h1>
+    <p>{ `My First name is ${fName} and last name is ${lName} `}</p>
   </>,
 
  
