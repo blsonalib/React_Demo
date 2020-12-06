4b74a4df5727faf6c4846eaa885518a1120ecb72
sonali <blsonalib@gmail.com>
2020-12-06 17:55:24 +0530

    Added javascript expression in react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const myElement = (
-  <>
-      <h1>This my Faviourite Social Media Application</h1>
-    <p>Some Websites are</p>
-    <ol>
-      <li>Facebook</li>
-      <li>Instagram</li>
-      <li>Whats app</li>
-      <li>Twitter</li>
-    </ol>
-  </> 
-);
-ReactDOM.render(
-  myElement, document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
+const fname = "sonali";
+const lName = "Bankar"
+
+ReactDOM.render(
+  <> 
+    <h1>My First_Name is {fname}</h1>
+    <h1>My Last_Name is { lName}</h1>
+  </>,
+
+ 
+  document.getElementById('root')
+);
