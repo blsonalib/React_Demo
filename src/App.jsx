51fbe749933cbf1e5b232c7f7dbb31d6d47211a7
sonali <blsonalib@gmail.com>
2020-12-08 23:30:40 +0530

    added Mini project by using component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-import Heading from "./Heading";
-import Paragraph from "./Paragraph";
-import List from "./List";
-
+// import "./index.css"
 function App() {
+  
+    let curTime = new Date(2020,11,8,10);
+    curTime = curTime.getHours();
+    let greeting = " ";
+    const cssColor = {};
+
+    if (curTime <= 12 && curTime <= 11) {
+        greeting = "Good Morning";
+        cssColor.color = "green";
+    } else if (curTime >= 12 && curTime <=17) {
+        greeting = "Good Afternoon";
+        cssColor.color = "orange";
+
+    } else {
+        greeting = "Good Night";
+        cssColor.color = "black";
+    }
+
     return (
         <>
-            <Heading />
-            <Paragraph />
-            <List />
+            <div className="box">
+                <h1>Hello sir, <span style= {cssColor}> { greeting } </span></h1>
+            </div>
         </>
-        
-    );
-    
-} export default App;
+    )
+}
+export default App;
