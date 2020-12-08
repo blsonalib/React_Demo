c930a9d91ad1a35eb54bed4d422dd6bf3cb0fd82
sonali <blsonalib@gmail.com>
2020-12-08 19:30:30 +0530

    Added inline ans internal css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
+const heading = {
+  color: "#f555fe",
+  marginTop: "20px",
+  textAlign: "center",
+  fontSize: "20px",
+  fontWeight: "500",
+  fontFamily: "'Poppins', sans-serif"
+};
+
+const imageBox = {
+  margin: "auto",
+  display: "flex",
+  justifyContent: "center"
+};
+
 var name = "Sonali";  
 var img1 = "https://picsum.photos/200/300";
 var img2 = "https://picsum.photos/200/270";
@@ -8,8 +23,10 @@ var img3 = "https://picsum.photos/300/300";
 var links = "https://www.facebook.com/"
 ReactDOM.render(
   <>
-    <h1 className="heading">I am {name}</h1>
-    <div className="image-box">
+    <h1
+      style={heading}
+    >I am {name}</h1>
+    <div style={imageBox} >
         <img src={img1} alt="This is image 1" />
         <img src={img2} alt="This is image 2" />
         <a href={ links} target="-blank">
